1048795f34e1f8a9dc3a57853ca764f484b84907
Nick Loewecke <konten@loewecke.de>
2022-03-21 18:09:14 +0100

    Add PollServiceTests

---
+++ Parlivote.Core.Tests.Unit/Services/Foundations/Polls/PollServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using Parlivote.Core.Brokers.Logging;
+using Parlivote.Core.Brokers.Storage;
+using Parlivote.Core.Services.Foundations.Polls;
+
+namespace Parlivote.Core.Tests.Unit.Services.Foundations.Polls;
+
+public class PollServiceTests
+{
+    private Mock<ILoggingBroker> loggingBrokerMock;
+    private Mock<IStorageBroker> storageBrokerMock;
+    private IPollService pollService;
+
+    public PollServiceTests()
+    {
+        this.loggingBrokerMock = new Mock<ILoggingBroker>();
+        this.storageBrokerMock = new Mock<IStorageBroker>();
+
+        this.pollService = new PollService(
+            this.loggingBrokerMock.Object,
+            this.storageBrokerMock.Object);
+    }
+
+
+}
--- Parlivote.Core/Services/Foundations/Polls/PollService.cs
+++ Parlivote.Core/Services/Foundations/Polls/PollService.cs
@@ -1,10 +1,20 @@
 ﻿using System.Threading.Tasks;
+using Parlivote.Core.Brokers.Logging;
+using Parlivote.Core.Brokers.Storage;
 using Parlivote.Shared.Models.Polls;
 
 namespace Parlivote.Core.Services.Foundations.Polls;
 
 public class PollService : IPollService
 {
+    private readonly ILoggingBroker loggingBroker;
+    private readonly IStorageBroker storageBroker;
+
+    public PollService(ILoggingBroker loggingBroker, IStorageBroker storageBroker)
+    {
+        this.loggingBroker = loggingBroker;
+        this.storageBroker = storageBroker;
+    }
     public Task<Poll> AddPollAsync(Poll poll)
     {
         throw new System.NotImplementedException();
