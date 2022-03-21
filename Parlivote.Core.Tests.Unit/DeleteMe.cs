e749c8d166da615957679f56e00b8f14b07f2f70
Nick Loewecke <n.loewecke@door-sys.com>
2022-03-21 12:54:42 +0100

    Add project files.

---
+++ Parlivote.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Parlivote.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()
+        {
+            
+        }
+    }
+}
