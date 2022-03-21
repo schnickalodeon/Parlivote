93e15821968d81e82d0bb931c29d5be7cf6fde7d
Nick Loewecke <konten@loewecke.de>
2022-03-21 17:00:37 +0100

    Adds StorageBroker to Startup and appsettings

--- Parlivote.Core/Startup.cs
+++ Parlivote.Core/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Parlivote.Core.Brokers.Storage;
 
 namespace Parlivote.Core
 {
@@ -25,6 +26,15 @@ namespace Parlivote.Core
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Parlivote.Core", Version = "v1" });
             });
+
+            AddBrokers(services);
+
+            services.AddDbContext<StorageBroker>();
+        }
+
+        private static void AddBrokers(IServiceCollection services)
+        {
+            services.AddTransient<IStorageBroker, StorageBroker>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
