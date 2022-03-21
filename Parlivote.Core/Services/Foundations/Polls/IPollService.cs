cdc5b6410cc2ed873c23277ff1d12e7657cdc0b6
Nick Loewecke <konten@loewecke.de>
2022-03-21 17:57:35 +0100

    Add PollService

---
+++ Parlivote.Core/Services/Foundations/Polls/IPollService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Parlivote.Shared.Models.Polls;
+
+namespace Parlivote.Core.Services.Foundations.Polls;
+
+public interface IPollService
+{
+    Task<Poll> AddPollAsync(Poll poll);
+}
---
+++ Parlivote.Core/Services/Foundations/Polls/PollService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Parlivote.Shared.Models.Polls;
+
+namespace Parlivote.Core.Services.Foundations.Polls;
+
+public class PollService : IPollService
+{
+    public Task<Poll> AddPollAsync(Poll poll)
+    {
+        throw new System.NotImplementedException();
+    }
+}
