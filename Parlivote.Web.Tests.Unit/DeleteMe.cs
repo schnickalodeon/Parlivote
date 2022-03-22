d91d91df9968aeb333e32940cf8946cc94b06993
Nick Loewecke <konten@loewecke.de>
2022-03-22 16:48:19 +0100

    Adds UnitTest-Project for Blazor

---
+++ Parlivote.Web.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Parlivote.Web.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
