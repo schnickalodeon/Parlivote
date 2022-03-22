023a20d9b8ef10e8316273c8ceaa4cc9bca325c1
Nick Loewecke <konten@loewecke.de>
2022-03-22 12:19:28 +0100

    ShouldThrowValidationException_OnAddIfPollIsNullAndLogItAsync -> FAIL

---
+++ Parlivote.Core.Tests.Unit/Services/Foundations/Polls/PollServiceTests.Validations.Add.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using Parlivote.Core.Brokers.Logging;
+using Parlivote.Core.Brokers.Storage;
+using Parlivote.Core.Services.Foundations.Polls;
+using Parlivote.Shared.Models.Polls;
+using Parlivote.Shared.Models.Polls.Exceptions;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace Parlivote.Core.Tests.Unit.Services.Foundations.Polls;
+
+public partial class PollServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowValidationException_OnAddIfPollIsNullAndLogItAsync()
+    {
+        // Arrange
+        Poll nullPoll = null;
+
+        var nullPollException =
+            new NullPollException();
+
+        var expectedPollValidationException =
+            new PollValidationException(nullPollException);
+
+        // Act
+        Task<Poll> addPollTask = this.pollService.AddAsync(nullPoll);
+         
+        // Assert
+        await Assert.ThrowsAsync<PollValidationException>(() => addPollTask);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.InsertPollAsync(It.IsAny<Poll>()),
+            Times.Never);
+
+        Tests.VerifyExceptionLogged(
+            this.loggingBrokerMock, 
+            expectedPollValidationException);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+    }
+}
---
+++ Parlivote.Shared/Models/Polls/Exceptions/PollValidationException.cs
@@ -0,0 +1,10 @@
+﻿using Xeptions;
+
+namespace Parlivote.Shared.Models.Polls.Exceptions;
+
+public class PollValidationException : Xeption
+{
+    public PollValidationException(Xeption innerException)
+    : base(message:"Poll validation error occurred, please try again")
+    { }
+}
