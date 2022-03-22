deafc9396a2f7d914e8c1e4ee469e832e66cb5c9
Nick Loewecke <konten@loewecke.de>
2022-03-22 12:58:31 +0100

    ShouldThrowValidationExceptionOnAddIfPollIsInvalidAndLogItAsync -> FAIL

--- Parlivote.Core.Tests.Unit/Services/Foundations/Polls/PollServiceTests.Validations.Add.cs
+++ Parlivote.Core.Tests.Unit/Services/Foundations/Polls/PollServiceTests.Validations.Add.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Moq;
 using Parlivote.Core.Brokers.Logging;
 using Parlivote.Core.Brokers.Storage;
 using Parlivote.Core.Services.Foundations.Polls;
+using Parlivote.Shared.Models;
 using Parlivote.Shared.Models.Polls;
 using Parlivote.Shared.Models.Polls.Exceptions;
 using Tynamix.ObjectFiller;
@@ -37,6 +39,51 @@ public partial class PollServiceTests
         Tests.VerifyExceptionLogged(
             this.loggingBrokerMock, 
             expectedPollValidationException);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task ShouldThrowValidationExceptionOnAddIfPollIsInvalidAndLogItAsync(string invalidText)
+    {
+        // Arrange
+        var invalidPoll = new Poll
+        {
+            Id = Guid.Empty,
+            Text = invalidText
+        };
+
+        var invalidPollException =
+            new InvalidPollException();
+
+        invalidPollException.AddData(
+            key: nameof(Poll.Id),
+            values: ExceptionMessages.INVALID_ID);
+
+        invalidPollException.AddData(
+            key: nameof(Poll.Text),
+            values: ExceptionMessages.INVALID_STRING);
+
+        var expectedPollValidationException
+            = new PollValidationException(invalidPollException);
+
+        // Act
+        Task<Poll> addPollTask = this.pollService.AddAsync(invalidPoll);
+
+        // Assert
+        await Assert.ThrowsAsync<PollValidationException>(() => addPollTask);
+
+        Tests.VerifyExceptionLogged(
+            this.loggingBrokerMock,
+            expectedPollValidationException);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.InsertPollAsync(It.IsAny<Poll>()),
+            Times.Never);
 
         this.loggingBrokerMock.VerifyNoOtherCalls();
         this.storageBrokerMock.VerifyNoOtherCalls();
---
+++ Parlivote.Shared/Models/ExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Parlivote.Shared.Models;
+
+public static class ExceptionMessages
+{
+    public const string INVALID_ID = "Die Id ist ein Pflichtfeld!";
+    public const string INVALID_STRING = "Text darf nicht leer sein!";
+    public const string DATE_TO_SMALLER_THAN_FROM = "Das Datum von darf nicht hinter dem Datum Bis liegen!";
+}
