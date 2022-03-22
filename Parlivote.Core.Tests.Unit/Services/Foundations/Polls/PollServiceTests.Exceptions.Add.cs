b6ec0595281e8cbb54e0ab5412c78c74bef8e1e9
Nick Loewecke <konten@loewecke.de>
2022-03-22 11:30:42 +0100

    ThrowsCriticalDependencyException_OnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Parlivote.Core.Tests.Unit/Services/Foundations/Polls/PollServiceTests.Exceptions.Add.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
+using Moq;
+using Parlivote.Shared.Models.Polls;
+using Parlivote.Shared.Models.Polls.Exceptions;
+using Xunit;
+
+namespace Parlivote.Core.Tests.Unit.Services.Foundations.Polls;
+
+public partial class PollServiceTests
+{
+    [Fact]
+    public async Task ThrowsCriticalDependencyException_OnAddIfSqlErrorOccursAndLogItAsync()
+    {
+        // Arrange
+        Poll somePoll = GetRandomPoll();
+        Poll inputPoll = somePoll;
+
+        SqlException sqlException = Tests.GetSqlException();
+
+        var pollStorageException =
+            new FailedPollStorageException(sqlException);
+
+        var expectedPollDependencyException =
+            new PollDependencyException(pollStorageException);
+
+        this.storageBrokerMock.Setup(broker =>
+            broker.InsertPollAsync(It.IsAny<Poll>()))
+                .ThrowsAsync(sqlException);
+
+        // Act
+        Task<Poll> addPollTask =
+            this.pollService.AddPollAsync(inputPoll);
+
+        // Assert
+        await Assert.ThrowsAsync<PollDependencyException>(() => addPollTask);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.InsertPollAsync(inputPoll),
+            Times.Once);
+
+        Tests.VerifyCriticalExceptionLogged(this.loggingBrokerMock, expectedPollDependencyException);
+
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+    }
+}
---
+++ Parlivote.Core.Tests.Unit/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Parlivote.Core.Brokers.Logging;
+using Parlivote.Shared.Models.Polls.Exceptions;
+using Xeptions;
+
+namespace Parlivote.Core.Tests.Unit;
+
+public static class Tests
+{
+    public static SqlException GetSqlException() =>
+        (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
+
+    public static Expression<Func<Exception, bool>> SameExceptionAs(Xeption expectedException)
+    {
+        return actualException =>
+            actualException.Message == expectedException.Message &&
+            actualException.InnerException.Message == expectedException.InnerException.Message &&
+            (actualException.InnerException as Xeption).DataEquals(expectedException.InnerException.Data) &&
+            (expectedException.InnerException as Xeption).DataEquals(actualException.InnerException.Data);
+    }
+
+    public static void VerifyCriticalExceptionLogged(Mock<ILoggingBroker> loggingBrokerMock, Xeption expectedException)
+    {
+        loggingBrokerMock.Verify(broker =>
+            broker.LogCritical(It.Is(SameExceptionAs(expectedException))),
+            Times.Once);
+    }
+}
