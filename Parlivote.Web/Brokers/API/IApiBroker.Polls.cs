2e85283d4e236fab5abf1948f5323e7f342d88c6
Nick Loewecke <konten@loewecke.de>
2022-03-22 17:48:08 +0100

    Adds ApiBroker for Polls

---
+++ Parlivote.Web/Brokers/API/IApiBroker.Polls.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Parlivote.Shared.Models.Polls;
+
+namespace Parlivote.Web.Brokers.API;
+
+public partial interface IApiBroker
+{
+    Task<Poll> PostPollAsync(Poll poll);
+    Task<List<Poll>>GetAllPollsAsync();
+    Task<Poll> GetPollById(Guid pollId);
+    Task<Poll> PutPollAsync(Poll poll);
+    Task<Poll> DeletePollById(Guid pollId);
+}
