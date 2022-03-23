537b4ee1880dbbe9a0c0b0a78638531084e39d71
Nick Loewecke <konten@loewecke.de>
2022-03-23 17:42:03 +0100

    ShouldThrowValidationExceptionOnAddIfPollIsInvalidAndLogItAsync -> PASS

--- Parlivote.Web/Services/Foundations/Polls/PollService.Validations.cs
+++ Parlivote.Web/Services/Foundations/Polls/PollService.Validations.cs
@@ -10,6 +10,11 @@ public partial class PollService
     private void ValidatePoll(Poll poll)
     {
         ValidatePollIsNotNull(poll);
+
+        Validate(
+            (IsInvalid(poll.Id), nameof(Poll.Id)),
+            (IsInvalid(poll.Text), nameof(Poll.Text))
+        );
     }
 
     private void ValidatePollIsNotNull(Poll poll)
@@ -19,6 +24,33 @@ public partial class PollService
             throw new NullPollException();
         }
     }
+    private static dynamic IsInvalid(Guid id) => new
+    {
+        Condition = id == Guid.Empty,
+        Message = ExceptionMessages.INVALID_ID
+    };
 
-   
+    private static dynamic IsInvalid(string text) => new
+    {
+        Condition = String.IsNullOrWhiteSpace(text),
+        Message = ExceptionMessages.INVALID_STRING
+    };
+
+    private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+    {
+        var invalidPostException = new InvalidPollException();
+
+        foreach ((dynamic rule, string parameter) in validations)
+        {
+            if (rule.Condition)
+            {
+                invalidPostException.UpsertDataList(
+                    key: parameter,
+                    value: rule.Message);
+            }
+        }
+
+        invalidPostException.ThrowIfContainsErrors();
+    }
+
 }
