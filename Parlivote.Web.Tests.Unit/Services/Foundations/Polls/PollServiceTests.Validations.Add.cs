c4ab3da7c769e7730a8f6fcb126f6feea9725d4b
Nick Loewecke <konten@loewecke.de>
2022-03-23 17:38:49 +0100

    ShouldThrowValidationExceptionOnAddIfPollIsInvalidAndLogItAsync -> FAIL

--- Parlivote.Web.Tests.Unit/Services/Foundations/Polls/PollServiceTests.Validations.Add.cs
+++ Parlivote.Web.Tests.Unit/Services/Foundations/Polls/PollServiceTests.Validations.Add.cs
@@ -39,4 +39,50 @@ public partial class PollServiceTests
         this.loggingBrokerMock.VerifyNoOtherCalls();
         this.apiBrokerMock.VerifyNoOtherCalls();
     }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task ShouldThrowValidationExceptionOnAddIfPollIsInvalidAndLogItAsync(string invalidText)
+    {
+        // Arrange
+        var invalidPoll = new Poll
+        {
+            Id = Guid.Empty,
+            Text = invalidText
+        };
+
+        var invalidPollException =
+            new InvalidPollException();
+
+        invalidPollException.AddData(
+            key: nameof(Poll.Id),
+            values: ExceptionMessages.INVALID_ID);
+
+        invalidPollException.AddData(
+            key: nameof(Poll.Text),
+            values: ExceptionMessages.INVALID_STRING);
+
+        var expectedPollValidationException
+            = new PollValidationException(invalidPollException);
+
+        // Act
+        Task<Poll> addPollTask = this.pollService.AddAsync(invalidPoll);
+
+        // Assert
+        await Assert.ThrowsAsync<PollValidationException>(() => addPollTask);
+
+        Tests.VerifyExceptionLogged(
+            this.loggingBrokerMock,
+            expectedPollValidationException);
+
+        this.apiBrokerMock.Verify(broker =>
+            broker.PostPollAsync(It.IsAny<Poll>()),
+            Times.Never);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.apiBrokerMock.VerifyNoOtherCalls();
+    }
+
 }
