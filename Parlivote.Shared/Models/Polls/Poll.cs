c788890c442e00a233435392e32e3f17b2932a37
Nick Loewecke <konten@loewecke.de>
2022-03-25 20:16:19 +0100

    Removes Poll Again

--- Parlivote.Shared/Models/Polls/Poll.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Parlivote.Shared.Models.Polls
-{
-    public class Poll
-    {
-        public Guid Id { get; set; }
-        public string AgendaItem { get; set; }
-        public string Text { get; set; }
-    }
-}
