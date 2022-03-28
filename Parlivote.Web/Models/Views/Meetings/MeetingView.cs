d5af2e20e221d10ec6cd52701ad0ccf6a4d206e7
Nick Loewecke <konten@loewecke.de>
2022-03-28 22:47:36 +0200

    Adds MeetingView

---
+++ Parlivote.Web/Models/Views/Meetings/MeetingView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Parlivote.Web.Models.Views.Meetings
+{
+    public class MeetingView
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset Start { get; set; }
+    }
+}
