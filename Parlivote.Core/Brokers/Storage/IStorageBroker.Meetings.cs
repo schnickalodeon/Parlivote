d4db47237a934a6c071498a27cdd37f299754ca2
Nick Loewecke <konten@loewecke.de>
2022-03-28 21:00:43 +0200

    Adds Meeting StorageBroker

---
+++ Parlivote.Core/Brokers/Storage/IStorageBroker.Meetings.cs
@@ -0,0 +1,15 @@
+﻿using Parlivote.Shared.Models.Meetings;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parlivote.Core.Brokers.Storage;
+
+public partial interface IStorageBroker
+{
+    Task<Meeting> InsertMeetingAsync(Meeting meeting);
+    IQueryable<Meeting> SelectAllMeetings();
+    Task<Meeting> SelectMeetingById(Guid meetingId);
+    Task<Meeting> UpdateMeetingAsync(Meeting meeting);
+    Task<Meeting> DeleteMeetingAsync(Meeting meeting);
+}
---
+++ Parlivote.Core/Brokers/Storage/StorageBroker.Meetings.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Parlivote.Shared.Models.Meetings;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parlivote.Core.Brokers.Storage;
+
+public partial class StorageBroker
+{
+    public DbSet<Meeting> Meetings { get; set; }
+    public async Task<Meeting> InsertMeetingAsync(Meeting meeting)
+    {
+        await using var broker = new StorageBroker(this.configuration);
+
+        EntityEntry<Meeting> addedEntityEntry =
+            await broker.Meetings.AddAsync(meeting);
+
+        await broker.SaveChangesAsync();
+
+        return addedEntityEntry.Entity;
+    }
+    public IQueryable<Meeting> SelectAllMeetings()
+    {
+        using var broker = new StorageBroker(this.configuration);
+        return broker.Meetings;
+    }
+    public async Task<Meeting> SelectMeetingById(Guid meetingId)
+    {
+        await using var broker = new StorageBroker(this.configuration);
+        return await broker.Meetings.FindAsync(meetingId);
+    }
+    public async Task<Meeting> UpdateMeetingAsync(Meeting meeting)
+    {
+        await using var broker = new StorageBroker(this.configuration);
+
+        EntityEntry<Meeting> updatedEntityEntry =
+            broker.Meetings.Update(meeting);
+
+        await broker.SaveChangesAsync();
+
+        return updatedEntityEntry.Entity;
+    }
+    public async Task<Meeting> DeleteMeetingAsync(Meeting meeting)
+    {
+        await using var broker = new StorageBroker(this.configuration);
+
+        EntityEntry<Meeting> deletedEntityEntry =
+            broker.Meetings.Remove(meeting);
+
+        await broker.SaveChangesAsync();
+
+        return deletedEntityEntry.Entity;
+    }
+}
