eccc46dfa8c0dae3f94aea845b987d3e3012be62
Nick Loewecke <konten@loewecke.de>
2022-03-28 20:57:24 +0200

    Adds Meeting-Model

---
+++ Parlivote.Shared/Models/Meetings/Meeting.cs
@@ -0,0 +1,8 @@
+﻿namespace Parlivote.Shared.Models.Meetings;
+
+public class Meeting
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public DateTimeOffset Start { get; set; }
+}
