85d801132876dbdaa3cfe0e6a96bd103ad5d89e2
Nick Loewecke <konten@loewecke.de>
2022-03-28 22:26:36 +0200

    Adds MeetingService Tests for Meeting in Blazor

---
+++ Parlivote.Web.Tests.Unit/Services/Foundations/Meetings/MeetingServiceTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
+using Moq;
+using Parlivote.Shared.Models.Meetings;
+using Parlivote.Web.Brokers.API;
+using Parlivote.Web.Brokers.Logging;
+using Parlivote.Web.Services.Foundations.Meetings;
+using RESTFulSense.Exceptions;
+using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
+
+namespace Parlivote.Web.Tests.Unit.Services.Foundations.Meetings;
+
+public partial class MeetingServiceTests
+{
+    private readonly Mock<ILoggingBroker> loggingBrokerMock;
+    private readonly Mock<IApiBroker> apiBrokerMock;
+    private readonly IMeetingService meetingService;
+    public MeetingServiceTests()
+    {
+        this.loggingBrokerMock = new Mock<ILoggingBroker>();
+        this.apiBrokerMock = new Mock<IApiBroker>();
+
+        this.meetingService = new MeetingService(
+            this.loggingBrokerMock.Object,
+            this.apiBrokerMock.Object);
+    }
+    private static Meeting GetRandomMeeting() =>
+        GetMeetingFiller(dates: Tests.GetRandomDateTimeOffset()).Create();
+
+    private static Filler<Meeting> GetMeetingFiller(DateTimeOffset dates)
+    {
+        var filler = new Filler<Meeting>();
+
+        filler.Setup()
+            .OnType<DateTimeOffset>().Use(dates);
+
+        return filler;
+    }
+
+    public static TheoryData DependencyException()
+    {
+        string randomMessage = Tests.GetRandomString();
+        var responseMessage = new HttpResponseMessage();
+
+        var httpResponseException =
+            new HttpResponseException(
+                httpResponseMessage: responseMessage,
+                message: randomMessage);
+
+        var httpResponseInternalServerErrorException =
+            new HttpResponseInternalServerErrorException(
+                responseMessage: responseMessage,
+                message: randomMessage);
+
+        return new TheoryData<Xeption>
+        {
+            httpResponseException,
+            httpResponseInternalServerErrorException
+        };
+    }
+
+    public static TheoryData CriticalDependencyException()
+    {
+        string randomMessage = Tests.GetRandomString();
+        var responseMessage = new HttpResponseMessage();
+
+        var urlNotFounException =
+            new HttpResponseUrlNotFoundException(
+                responseMessage: responseMessage,
+                message: randomMessage);
+
+        var unauthorizedException =
+            new HttpResponseUnauthorizedException(
+                responseMessage: responseMessage,
+                message: randomMessage);
+
+        return new TheoryData<Xeption>
+        {
+            urlNotFounException,
+            unauthorizedException
+        };
+    }
+
+    public static TheoryData DependencyValidationException()
+    {
+        string randomMessage = Tests.GetRandomString();
+        var responseMessage = new HttpResponseMessage();
+
+        var badRequestException =
+            new HttpResponseBadRequestException(
+                responseMessage: responseMessage,
+                message: randomMessage);
+
+        var conflictException =
+            new HttpResponseConflictException(
+                responseMessage: responseMessage,
+                message: randomMessage);
+
+        return new TheoryData<Xeption>
+        {
+            badRequestException,
+            conflictException
+        };
+    }
+}
---
+++ Parlivote.Web/Services/Foundations/Meetings/MeetingService.Exceptions.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Parlivote.Shared.Models.Meetings;
+using Parlivote.Shared.Models.Meetings.Exceptions;
+using RESTFulSense.Exceptions;
+using Xeptions;
+
+namespace Parlivote.Web.Services.Foundations.Meetings;
+
+public partial class MeetingService
+{
+    private delegate Task<Meeting> ReturningMeetingFunction();
+    private delegate Task<List<Meeting>> ReturningMeetingsFunction();
+
+    private async Task<Meeting> TryCatch(ReturningMeetingFunction returningMeetingFunction)
+    {
+        try
+        {
+            return await returningMeetingFunction();
+        }
+        catch (NullMeetingException nullMeetingException)
+        {
+            throw CreateAndLogValidationException(nullMeetingException);
+        }
+        catch (InvalidMeetingException invalidMeetingException)
+        {
+            throw CreateAndLogValidationException(invalidMeetingException);
+        }
+        catch (HttpResponseUrlNotFoundException httpResponseUrlNotFoundException)
+        {
+            throw CreateAndLogCriticalDependencyException(httpResponseUrlNotFoundException);
+        }
+        catch (HttpResponseUnauthorizedException httpUnauthorizedException)
+        {
+            throw CreateAndLogCriticalDependencyException(httpUnauthorizedException);
+        }
+        catch (HttpResponseConflictException httpConflictException)
+        {
+            throw CreateAndLogDependencyValidationException(httpConflictException);
+        }
+        catch (HttpResponseBadRequestException httpBadRequestException)
+        {
+            throw CreateAndLogDependencyValidationException(httpBadRequestException);
+        }
+        catch (HttpResponseInternalServerErrorException httpInternalServerErrorException)
+        {
+            throw CreateAndLogDependencyException(httpInternalServerErrorException);
+        }
+        catch (HttpResponseException httpResponseException)
+        {
+            throw CreateAndLogDependencyException(httpResponseException);
+        }
+        catch (Exception exception)
+        {
+            var failedMeetingServiceException =
+                new FailedMeetingServiceException(exception);
+
+            throw CreateAndLogServiceException(failedMeetingServiceException);
+        }
+    }
+    private async Task<List<Meeting>> TryCatch(ReturningMeetingsFunction returningMeetingsFunction)
+    {
+        try
+        {
+            return await returningMeetingsFunction();
+        }
+        catch (HttpResponseUrlNotFoundException httpResponseUrlNotFoundException)
+        {
+            throw CreateAndLogCriticalDependencyException(httpResponseUrlNotFoundException);
+        }
+        catch (HttpResponseUnauthorizedException httpUnauthorizedException)
+        {
+            throw CreateAndLogCriticalDependencyException(httpUnauthorizedException);
+        }
+        catch (HttpResponseInternalServerErrorException httpInternalServerErrorException)
+        {
+            throw CreateAndLogDependencyException(httpInternalServerErrorException);
+        }
+        catch (HttpResponseException httpResponseException)
+        {
+            throw CreateAndLogDependencyException(httpResponseException);
+        }
+        catch (Exception exception)
+        {
+            var failedMeetingServiceException =
+                new FailedMeetingServiceException(exception);
+
+            throw CreateAndLogServiceException(failedMeetingServiceException);
+        }
+    }
+    private MeetingValidationException CreateAndLogValidationException(Xeption exception)
+    {
+        var meetingValidationException =
+            new MeetingValidationException(exception);
+
+        this.loggingBroker.LogError(meetingValidationException);
+
+        return meetingValidationException;
+    }
+    private MeetingDependencyValidationException CreateAndLogDependencyValidationException(Xeption exception)
+    {
+        var meetingDependencyValidationException =
+            new MeetingDependencyValidationException(exception);
+
+        this.loggingBroker.LogError(meetingDependencyValidationException);
+
+        return meetingDependencyValidationException;
+    }
+    private MeetingDependencyException CreateAndLogDependencyException(Xeption exception)
+    {
+        var meetingDependencyException =
+            new MeetingDependencyException(exception);
+
+        this.loggingBroker.LogError(meetingDependencyException);
+
+        return meetingDependencyException;
+    }
+    private MeetingDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+    {
+        var meetingDependencyException =
+            new MeetingDependencyException(exception);
+
+        this.loggingBroker.LogCritical(meetingDependencyException);
+
+        return meetingDependencyException;
+    }
+    private MeetingServiceException CreateAndLogServiceException(Xeption exception)
+    {
+        var meetingServiceException =
+            new MeetingServiceException(exception);
+
+        this.loggingBroker.LogError(meetingServiceException);
+
+        return meetingServiceException;
+    }
+}
