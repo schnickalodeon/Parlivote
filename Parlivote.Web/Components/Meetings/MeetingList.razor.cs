e698f935e018be57c40eda23c1c84f55f785ebd1
Nick Loewecke <konten@loewecke.de>
2022-03-30 12:05:34 +0200

    Adds Meeting List Component

---
+++ Parlivote.Web/Components/Meetings/MeetingList.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Parlivote.Web.Models;
+using Parlivote.Web.Models.Views.Meetings;
+using Parlivote.Web.Models.Views.Motions;
+using Parlivote.Web.Services.Views.Meetings;
+
+namespace Parlivote.Web.Components.Meetings;
+
+public partial class MeetingList : ComponentBase
+{
+    [Inject]
+    public IMeetingViewService MeetingViewService { get; set; }
+
+    private ComponentState state;
+    private List<MeetingView> meetings;
+    private string error;
+
+    protected override async Task OnInitializedAsync()
+    {
+        this.state = ComponentState.Loading;
+        await LoadMeetings();
+    }
+
+    private async Task LoadMeetings()
+    {
+        try
+        {
+            this.meetings =
+                await this.MeetingViewService.GetAllAsync();
+            this.state = ComponentState.Content;
+        }
+        catch (Exception e)
+        {
+            this.error = e.InnerException?.Message;
+            this.state = ComponentState.Error;
+        }
+    }
+}
