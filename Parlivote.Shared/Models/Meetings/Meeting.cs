be3a20e871108e0fc091fd6cb76cb2d37172b38c
Nick Loewecke <konten@loewecke.de>
2022-03-30 13:00:38 +0200

    Conntect Motions and Meetings

--- Parlivote.Shared/Models/Meetings/Meeting.cs
+++ Parlivote.Shared/Models/Meetings/Meeting.cs
@@ -1,8 +1,11 @@
-﻿namespace Parlivote.Shared.Models.Meetings;
+﻿using Parlivote.Shared.Models.Motions;
+
+namespace Parlivote.Shared.Models.Meetings;
 
 public class Meeting
 {
     public Guid Id { get; set; }
     public string Description { get; set; }
     public DateTimeOffset Start { get; set; }
+    private ICollection<Motion> Motions { get; set; }
 }
