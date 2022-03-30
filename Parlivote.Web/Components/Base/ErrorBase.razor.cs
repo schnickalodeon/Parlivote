104da081e4c955a6b58108116f315b532fe331bf
Nick Loewecke <konten@loewecke.de>
2022-03-30 20:47:17 +0200

    Adds ErrorBase, TextBoxBase and ValidationSummaryBase

---
+++ Parlivote.Web/Components/Base/ErrorBase.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Parlivote.Web.Components.Base
+{
+    public partial class ErrorBase : ComponentBase
+    {
+        [Parameter] 
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ Parlivote.Web/Components/Base/ValidationSummaryBase.razor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Parlivote.Web.Models;
+
+namespace Parlivote.Web.Components.Base;
+
+public partial class ValidationSummaryBase : ComponentBase
+{
+    [Parameter]
+    public IDictionary ValidationErrors { get; set; }
+
+    private Dictionary<string, string> validationDictionary;
+    private ComponentState state;
+
+    protected override void OnInitialized()
+    {
+        this.state = ComponentState.Loading;
+    }
+
+    protected override void OnParametersSet()
+    {
+        SetValidationDictionary();
+    }
+
+    private void SetValidationDictionary()
+    {
+        this.validationDictionary = new Dictionary<string, string>();
+        foreach(DictionaryEntry validationError in this.ValidationErrors)
+        {
+            string parameter = validationError.Key.ToString();
+            var value = validationError.Value;
+            string[] messages = value as string[];
+
+            if (messages != null && messages.Length > 0)
+            {
+                string joinedMessages = string.Join(",", messages);
+                this.validationDictionary.Add(parameter, joinedMessages);
+            }
+            else
+            {
+                this.validationDictionary.Add(parameter,"");
+            }
+        }
+        this.state = ComponentState.Content;
+    }
+}
