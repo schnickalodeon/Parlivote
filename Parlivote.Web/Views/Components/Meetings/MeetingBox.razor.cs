452a8042b14ed674a43caeb5e38cda4e390b7f82
Nick Loewecke <konten@loewecke.de>
2022-03-31 16:31:29 +0200

    Adds MeetingBox and use it in MeetingList

---
+++ Parlivote.Web/Views/Components/Meetings/MeetingBox.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Parlivote.Web.Models.Views.Meetings;
+
+namespace Parlivote.Web.Views.Components.Meetings;
+
+public partial class MeetingBox : ComponentBase
+{
+    [Parameter]
+    public MeetingView Meeting { get; set; }
+}
