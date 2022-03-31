02312163e04b174eb07d9be090bd0f309464d464
Nick Loewecke <konten@loewecke.de>
2022-03-31 16:31:17 +0200

    Adds Acordion Base

---
+++ Parlivote.Web/Views/Base/Accordion.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Parlivote.Web.Views.Base;
+
+public partial class Accordion
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    public Guid id = Guid.NewGuid();
+}
