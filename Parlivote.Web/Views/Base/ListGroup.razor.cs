e7eadee45fbdf757be3ae48269eed06c1adbda4a
Nick Loewecke <konten@loewecke.de>
2022-03-31 21:25:31 +0200

    Add ListGroupBase Items

---
+++ Parlivote.Web/Views/Base/ListGroup.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Parlivote.Web.Views.Base;
+
+public partial class ListGroup : ComponentBase
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+}
---
+++ Parlivote.Web/Views/Base/ListGroupItem.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Parlivote.Web.Views.Base;
+
+public partial class ListGroupItem : ComponentBase
+{
+    [Parameter]
+    public RenderFragment Title { get; set; }
+
+    [Parameter]
+    public RenderFragment Body { get; set; }
+
+    [Parameter] 
+    public RenderFragment Additional { get; set; }
+}
