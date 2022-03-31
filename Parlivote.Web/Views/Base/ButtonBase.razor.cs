6745b9204c538de301fa364d4efeffb2aba33cf5
Nick Loewecke <konten@loewecke.de>
2022-03-31 11:30:57 +0200

    Adds ButtonBase and Fixed DialogBase

---
+++ Parlivote.Web/Views/Base/ButtonBase.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Parlivote.Shared.Extensions;
+
+namespace Parlivote.Web.Views.Base
+{
+    public partial class ButtonBase : ComponentBase
+    {
+        [Parameter] public string Text { get; set; }
+
+        [Parameter] public Action OnClick { get; set; }
+
+        [Parameter] public string CustomCss { get; set; }
+
+        private string css;
+        protected override void OnParametersSet()
+        {
+            this.css = CustomCss.IsNullOrWhitespace() 
+                ? "btn btn-primary"
+                : CustomCss;
+        }
+    }
+}
