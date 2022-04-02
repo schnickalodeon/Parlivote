477cad4ab681e373a6bf112d40434a256d4e2cb4
Nick Loewecke <konten@loewecke.de>
2022-04-02 13:45:54 +0200

    ThrowsCriticalDependencyException_OnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Parlivote.Core.Tests.Unit/Services/Foundations/Meetings/MeetingServiceTests.Exceptions.Modfiy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
+using FluentAssertions;
+using Force.DeepCloner;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using Moq;
+using Parlivote.Shared.Models.Meetings;
+using Parlivote.Shared.Models.Meetings.Exceptions;
+using Xunit;
+
+namespace Parlivote.Core.Tests.Unit.Services.Foundations.Meetings;
+
+public partial class MeetingServiceTests
+{
+    [Fact]
+    public async Task ThrowsCriticalDependencyException_OnModifyIfSqlErrorOccursAndLogItAsync()
+    {
+        // Arrange
+        Meeting someMeeting = GetRandomMeeting();
+        Meeting inputMeeting = someMeeting;
+
+        SqlException sqlException = Tests.GetSqlException();
+
+        var meetingStorageException =
+            new FailedMeetingStorageException(sqlException);
+
+        var expectedMeetingDependencyException =
+            new MeetingDependencyException(meetingStorageException);
+
+        this.storageBrokerMock.Setup(broker =>
+            broker.SelectMeetingById(It.IsAny<Guid>()))
+                .ThrowsAsync(sqlException);
+
+        // Act
+        Task<Meeting> addMeetingTask =
+            this.meetingService.ModifyAsync(inputMeeting);
+
+        // Assert
+        await Assert.ThrowsAsync<MeetingDependencyException>(() => addMeetingTask);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.SelectMeetingById(inputMeeting.Id),
+            Times.Once);
+
+        Tests.VerifyCriticalExceptionLogged(
+            this.loggingBrokerMock, 
+            expectedMeetingDependencyException);
+
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+    }
+}
