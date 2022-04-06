ded9f7f5ff5553aaabe0a1b94e0c4e6a860f726b
Nick Loewecke <konten@loewecke.de>
2022-04-06 14:50:41 +0200

    Modify Motion Validation -> FAILED

---
+++ Parlivote.Core.Tests.Unit/Services/Foundations/Motions/MotionServiceTests.Validations.Modify.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Parlivote.Core.Brokers.Logging;
+using Parlivote.Core.Brokers.Storage;
+using Parlivote.Core.Services.Foundations.Motions;
+using Parlivote.Shared.Models;
+using Parlivote.Shared.Models.Motions;
+using Parlivote.Shared.Models.Motions.Exceptions;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace Parlivote.Core.Tests.Unit.Services.Foundations.Motions;
+
+public partial class MotionServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowValidationException_OnModifyIfMotionIsNullAndLogItAsync()
+    {
+        // Arrange
+        Motion nullMotion = null;
+
+        var nullMotionException =
+            new NullMotionException();
+
+        var expectedMotionValidationException =
+            new MotionValidationException(nullMotionException);
+
+        // Act
+        Task<Motion> modifyMotionTask = this.motionService.ModifyAsync(nullMotion);
+         
+        // Assert
+        await Assert.ThrowsAsync<MotionValidationException>(() => modifyMotionTask);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.InsertMotionAsync(It.IsAny<Motion>()),
+            Times.Never);
+
+        Tests.VerifyExceptionLogged(
+            this.loggingBrokerMock, 
+            expectedMotionValidationException);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task ShouldThrowValidationExceptionOnModifyIfMotionIsInvalidAndLogItAsync(string invalidText)
+    {
+        // Arrange
+        var invalidMotion = new Motion
+        {
+            Id = Guid.Empty,
+            Text = invalidText,
+            Version = Tests.GetRandomNegativeNumber()
+        };
+
+        var invalidMotionException =
+            new InvalidMotionException();
+
+        invalidMotionException.AddData(
+            key: nameof(Motion.Id),
+            values: ExceptionMessages.INVALID_ID);
+
+        invalidMotionException.AddData(
+            key: nameof(Motion.Text),
+            values: ExceptionMessages.INVALID_STRING);
+
+        invalidMotionException.AddData(
+            key: nameof(Motion.Version),
+            values: ExceptionMessages.Motions.INVALID_VERSION);
+
+        var expectedMotionValidationException
+            = new MotionValidationException(invalidMotionException);
+
+        // Act
+        Task<Motion> modifyMotionTask = this.motionService.ModifyAsync(invalidMotion);
+
+        // Assert
+        await Assert.ThrowsAsync<MotionValidationException>(() => modifyMotionTask);
+
+        Tests.VerifyExceptionLogged(
+            this.loggingBrokerMock,
+            expectedMotionValidationException);
+
+        this.storageBrokerMock.Verify(broker =>
+                broker.InsertMotionAsync(It.IsAny<Motion>()),
+            Times.Never);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnModifyIfMotionDoesNotExistAndLogItAsync()
+    {
+        // given
+        Motion randomMotion = GetRandomMotion();
+        Motion nonExistMotion = randomMotion;
+        Motion nullMotion = null;
+
+        var notFoundMotionException =
+            new NotFoundMotionException(nonExistMotion.Id);
+
+        var expectedMotionValidationException =
+            new MotionValidationException(notFoundMotionException);
+
+        this.storageBrokerMock.Setup(broker =>
+            broker.SelectMotionById(nonExistMotion.Id))
+                .ReturnsAsync(nullMotion);
+
+        // when 
+        Task<Motion> modifyMotionTask =
+            this.motionService.ModifyAsync(nonExistMotion);
+
+        // then
+        await Assert.ThrowsAsync<MotionValidationException>(() => modifyMotionTask);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.SelectMotionById(nonExistMotion.Id),
+            Times.Once);
+
+        Tests.VerifyExceptionLogged(this.loggingBrokerMock,
+            expectedMotionValidationException);
+
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+    }
+}
--- Parlivote.Shared/Models/ExceptionMessages.cs
+++ Parlivote.Shared/Models/ExceptionMessages.cs
@@ -5,4 +5,9 @@ public static class ExceptionMessages
     public const string INVALID_ID = "Die Id ist ein Pflichtfeld!";
     public const string INVALID_STRING = "Text darf nicht leer sein!";
     public const string DATE_TO_SMALLER_THAN_FROM = "Das Datum von darf nicht hinter dem Datum Bis liegen!";
+
+    public static class Motions
+    {
+        public const string INVALID_VERSION = "Die Version muss positiv sein";
+    }
 }
