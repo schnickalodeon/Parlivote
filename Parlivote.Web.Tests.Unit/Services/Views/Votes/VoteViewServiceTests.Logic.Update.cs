3dca33ed259824cac73a038e1db8e426927d788c
Nick Loewecke <konten@loewecke.de>
2022-05-12 21:04:18 +0200

    Adds ViewServices with Tests

---
+++ Parlivote.Web.Tests.Unit/Services/Views/Votes/VoteViewServiceTests.Logic.Update.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Parlivote.Shared.Models.Votes;
+using Parlivote.Shared.Models.VoteValues;
+using Parlivote.Web.Models.Views.Votes;
+using Xunit;
+
+namespace Parlivote.Web.Tests.Unit.Services.Views.Votes;
+
+public partial class VoteViewServiceTests
+{
+    [Fact]
+    public async Task ShouldUpdateVoteViewAsyncAsync()
+    {
+        // Arrange
+        dynamic someVoteViewInput = CreateRandomVoteView();
+
+        var someVoteView = new VoteView
+        {
+            VoteId = someVoteViewInput.Id,
+            MotionId = someVoteViewInput.MotionId,
+            UserId = someVoteViewInput.UserId,
+            Value = (VoteValue)someVoteViewInput.Value
+        };
+
+        VoteView inputVoteView = someVoteView;
+        VoteView expectedVoteView = inputVoteView;
+
+        var someVote = new Vote
+        {
+            Id = someVoteViewInput.Id,
+            MotionId = someVoteViewInput.MotionId,
+            UserId = someVoteViewInput.UserId,
+            Value = (VoteValue)someVoteViewInput.Value
+        };
+
+        Vote expectedInputVote = someVote;
+        Vote returnedVote = expectedInputVote;
+
+        this.voteServiceMock.Setup(service =>
+            service.ModifyAsync(It.IsAny<Vote>()))
+                .ReturnsAsync(returnedVote);
+
+        // Act
+        VoteView actualVoteView =
+            await this.voteViewService.UpdateAsync(inputVoteView);
+
+        // Assert
+        actualVoteView.Should().BeEquivalentTo(
+            expectedVoteView, 
+            options => options.Excluding(voteView => voteView.VoteId));
+
+        this.voteServiceMock.Verify(service =>
+            service.ModifyAsync(It.Is(SameVoteAs(expectedInputVote))),
+            Times.Once);
+    }
+}
---
+++ Parlivote.Web/Services/Views/Votes/IVoteViewService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Parlivote.Shared.Models.Votes;
+using Parlivote.Web.Models.Views.Votes;
+
+namespace Parlivote.Web.Services.Views.Votes;
+
+public interface IVoteViewService
+{
+    Task<VoteView> AddAsync(VoteView voteView);
+    Task<List<VoteView>> GetAllAsync();
+    Task<VoteView> GetByIdAsync(Guid voteId);
+    Task<VoteView> UpdateAsync(VoteView voteView);
+    Task<Vote> DeleteByIdAsync(Guid voteId);
+}
