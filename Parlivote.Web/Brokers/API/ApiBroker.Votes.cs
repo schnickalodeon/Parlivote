d4d62a6af95e9ee9ca79ac0b6a781c485fe216a1
Nick Loewecke <konten@loewecke.de>
2022-05-12 20:14:36 +0200

    Adds ApiBroker for Votes (with interface)

---
+++ Parlivote.Web/Brokers/API/ApiBroker.Votes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Parlivote.Shared.Models.Votes;
+
+namespace Parlivote.Web.Brokers.API;
+
+public partial class ApiBroker
+{
+    private const string VotesRelativeUrl = "/api/v1/votes";
+
+    public async Task<Vote> PostVoteAsync(Vote vote) =>
+        await this.PostAsync(VotesRelativeUrl, vote);
+    public async Task<List<Vote>> GetAllVotesAsync() =>
+        await this.GetAsync<List<Vote>>(VotesRelativeUrl);
+    public async Task<Vote> GetVoteById(Guid voteId) =>
+        await this.GetAsync<Vote>($"{VotesRelativeUrl}/{voteId}");
+    public async Task<Vote> PutVoteAsync(Vote vote) =>
+        await this.PutAsync(VotesRelativeUrl,vote);
+    public async Task<Vote> DeleteVoteById(Guid voteId) =>
+        await this.DeleteAsync<Vote>($"{VotesRelativeUrl}/{voteId}");
+}
---
+++ Parlivote.Web/Brokers/API/IApiBroker.Votes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Parlivote.Shared.Models.Votes;
+
+namespace Parlivote.Web.Brokers.API;
+
+public partial interface IApiBroker
+{
+    Task<Vote> PostVoteAsync(Vote vote);
+    Task<List<Vote>>GetAllVotesAsync();
+    Task<Vote> GetVoteById(Guid voteId);
+    Task<Vote> PutVoteAsync(Vote vote);
+    Task<Vote> DeleteVoteById(Guid voteId);
+}
