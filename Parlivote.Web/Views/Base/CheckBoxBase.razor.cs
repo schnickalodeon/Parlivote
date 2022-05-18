472df61f06f66b949c3f441213cdbc5ca42935fa
Nick Loewecke <konten@loewecke.de>
2022-05-18 22:46:48 +0200

    IMplements CheckBoxBase and MeetingAttendanceComponent

---
+++ Parlivote.Web/Views/Base/CheckBoxBase.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Parlivote.Web.Views.Base;
+
+public partial class CheckBoxBase : ComponentBase
+{
+    [Parameter]
+    public string Text { get; set; }
+
+    [Parameter]
+    public bool IsChecked { get; set; }
+}
---
+++ Parlivote.Web/Views/Components/Meetings/MeetingAttendanceComponent.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Parlivote.Web.Models.Views.Meetings;
+using Parlivote.Web.Services.Views.Meetings;
+
+namespace Parlivote.Web.Views.Components.Meetings;
+
+public partial class MeetingAttendanceComponent
+{
+    [Inject]
+    IMeetingViewService meetingViewService;
+
+    [Parameter]
+    public MeetingView MeetingView { get; set; }
+
+    private bool isAttendant;
+    public bool IsAttendant
+    {
+        get => this.isAttendant;
+        set
+        {
+            this.isAttendant = value;
+            UpdateAttendance(value);
+        }
+    }
+
+    private static void UpdateAttendance(bool attendance)
+    {
+        
+    }
+}
