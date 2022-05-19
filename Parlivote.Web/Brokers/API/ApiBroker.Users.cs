cb21a71bd578f6fbab3e97d613f275154de4cc06
Nick Loewecke <konten@loewecke.de>
2022-05-19 11:14:10 +0200

    Adds ApiBroker for Users with GetAll Method

---
+++ Parlivote.Web/Brokers/API/ApiBroker.Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Parlivote.Shared.Models.Identity.Users;
+
+namespace Parlivote.Web.Brokers.API;
+
+public partial class ApiBroker
+{
+    private const string UsersRelativeUrl = "/api/v1/users";
+    public async Task<List<User>> GetAllUsersAsync() =>
+        await this.GetAsync<List<User>>(UsersRelativeUrl);
+}
