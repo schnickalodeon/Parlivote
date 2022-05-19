5d503a20b8d07671bd6b6f495970f53dbcdc7a3c
Nick Loewecke <konten@loewecke.de>
2022-05-19 11:08:02 +0200

    Add UserController

---
+++ Parlivote.Core/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Parlivote.Core.Services.Foundations.Users;
+using Parlivote.Shared.Models.Identity.Users;
+using Parlivote.Shared.Models.Identity.Users.Exceptions;
+using RESTFulSense.Controllers;
+
+namespace Parlivote.Core.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UsersController : RESTFulController
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<IQueryable<User>> GetAllUsers()
+        {
+            try
+            {
+                IQueryable<User> users = this.userService.RetrieveAll();
+                return Ok(users);
+            }
+            catch (UserDependencyException pollDependencyException)
+            {
+                return InternalServerError(pollDependencyException);
+            }
+            catch (UserServiceException pollServiceException)
+            {
+                return InternalServerError(pollServiceException);
+            }
+        }
+    }
+}
