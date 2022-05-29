cdc0a90e311c49e17dfe72942ceedd66aa419557
Nick Loewecke <konten@loewecke.de>
2022-05-29 14:12:12 +0200

    Adds Migration for Moving Attendance to User

---
+++ Parlivote.Core/Migrations/20220529121100_MovesAttendanceToUser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parlivote.Core.Migrations
+{
+    public partial class MovesAttendanceToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendances");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAttendant",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLoggedIn",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAttendant",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsLoggedIn",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "Attendances",
+                columns: table => new
+                {
+                    AttendantUsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MeetingsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendances", x => new { x.AttendantUsersId, x.MeetingsId });
+                    table.ForeignKey(
+                        name: "FK_Attendances_AspNetUsers_AttendantUsersId",
+                        column: x => x.AttendantUsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Attendances_Meetings_MeetingsId",
+                        column: x => x.MeetingsId,
+                        principalTable: "Meetings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendances_MeetingsId",
+                table: "Attendances",
+                column: "MeetingsId");
+        }
+    }
+}
