a7127065ad45324ac1106e9e89aaf64ff08d08e5
Nick Loewecke <konten@loewecke.de>
2022-06-27 20:19:15 +0200

    Adds Title to motion

--- Parlivote.Core/Migrations/20220627180319_AddsTitleToMoiton.Designer.cs
+++ Parlivote.Core/Migrations/20220627180319_AddsTitleToMoiton.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Parlivote.Core.Brokers.Storage;
 
@@ -11,9 +12,10 @@ using Parlivote.Core.Brokers.Storage;
 namespace Parlivote.Core.Migrations
 {
     [DbContext(typeof(StorageBroker))]
-    partial class StorageBrokerModelSnapshot : ModelSnapshot
+    [Migration("20220627180319_AddsTitleToMoiton")]
+    partial class AddsTitleToMoiton
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Parlivote.Core/Migrations/20220627180319_AddsTitleToMoiton.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parlivote.Core.Migrations
+{
+    public partial class AddsTitleToMoiton : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Motions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Motions");
+        }
+    }
+}
